a1d8b5c67ab22780f7db69f6ee3a33574f58bc26
Adarsh Kumar Mishra <124551477+Adarshabinash@users.noreply.github.com>
2025-06-11 16:04:36 +0530

    frf

--- index.js
+++ index.js
@@ -2,8 +2,17 @@
  * @format
  */
 
+import 'react-native-gesture-handler'; // 👈 MUST be at the very top!
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import React from 'react';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
-AppRegistry.registerComponent(appName, () => App);
+const Root = () => (
+  <GestureHandlerRootView style={{flex: 1}}>
+    <App />
+  </GestureHandlerRootView>
+);
+
+AppRegistry.registerComponent(appName, () => Root);
