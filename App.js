c5495e3fe62f0294888422b6eb72f0a5e5afe336
Debaswini Mishra <115451804+Debaswinimishra@users.noreply.github.com>
2025-06-11 17:17:15 +0530

    app.js changes added

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import StackScreen from '../AwesomeProject/src/navigations/StackNavigator';
+// import StackScreen from '../AwesomeProject/src/navigations/StackNavigator';
+import StackScreen from '../TZ_Support_App/src/navigations/StackNavigator';
 
 const App = () => {
   return (
